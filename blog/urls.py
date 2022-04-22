2f7e548c62639bd1b112edbc819cf56a04ae12ca
PMSP31 <pmsp3108@gmail.com>
2022-04-22 11:23:26 +0700

    Update & Delete

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/',views.createPost, name='create'),
+    path('delete/<int:id_post>',views.deletePost, name='delete'),
+    path('update/<int:id_post>',views.updatePost, name='update'),
     re_path(r'^post/(?P<slugInput>[\w-]+)/$', views.detailPost, name='detail'),
     re_path(r'^category/(?P<categoryInput>[\w-]+)/$', views.categoryPost, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -29,11 +29,40 @@ def createPost(request):
 
     if request.method == 'POST' :
         if post_form.is_valid():
-            """ Post.objects.create(
-                title = post_form.cleaned_data.get('title'),
-                body = post_form.cleaned_data.get('body'),
-                category = post_form.cleaned_data.get('category'),
-            ) """
+            post_form.save()
+            return redirect('blog:index')
+        else :
+            error = post_form.errors
+            context['error'] = error
+
+    return render(request, "blog/create.html", context)
+
+def deletePost(request, id_post):
+    # filter data by id, delete data from id
+    Post.objects.filter(id = id_post).delete()
+    return redirect('blog:index')
+
+def updatePost(request, id_post):
+    # get post by id
+    update_post = Post.objects.get(id = id_post)
+    # get data post from id
+    data_post = {
+        "title" : update_post.title,
+        "body" : update_post.body,
+        "categoory" : update_post.category,
+    }
+    post_form = PostForm(request.POST or None, initial = data_post, instance = update_post)
+
+    context = {
+        "app_css" : 'blog/css/styles.css',
+        'title' : 'Update Blog | Kelas Terbuka',
+        'heading' : "Update Blog",
+        'subheading' : 'Update Blog on Kelas Terbuka',
+        'post_form' : post_form
+    }
+
+    if request.method == 'POST' :
+        if post_form.is_valid():
             post_form.save()
             return redirect('blog:index')
         else :
